c57dd3c93c5a3e5816e68ce574c27609ed37dbf3
Jaime <jaime@ejemplo.com>
2022-01-13 20:28:15 +0100

    tamaño máximo de subida de archivo añadido

--- DetalleIncidencia.aspx.cs
+++ DetalleIncidencia.aspx.cs
@@ -280,55 +280,63 @@ namespace ProyectoFinalDAM
             string extension = "";
             int size = 0;
             int flag = 0;
+            int tamMax = 2000;
 
             if (fuArchivo.HasFile == true)
             {
                 nombre = Path.GetFileNameWithoutExtension(fuArchivo.FileName);
                 extension = Path.GetExtension(fuArchivo.FileName);
                 size = fuArchivo.PostedFile.ContentLength;
-                switch (extension.ToLower())
+                if (size > tamMax/1024)
                 {
-                    case ".doc":
-                    case ".docx":
-                    case ".pdf":
-                    case ".png":
-                    case ".jpg":
-                    case ".txt":
-                        flag = 1;
-                        break;
-                    default:
-                        flag= 0;
-                        break;
-                }
-                if(flag == 1)
-                {
-                    string query = "INSERT INTO archivo (id_archivo, nombre, id_incidencia) VALUES (id_archivo,@nombre,@id_incidencia)";
-
-                    MySqlConnection conn = con.Conectar();
-                    MySqlCommand cmd = new MySqlCommand(query, conn);
-
-                    cmd.Parameters.AddWithValue("@nombre", nombre+extension);
-                    cmd.Parameters.AddWithValue("@id_incidencia", lb_IdIncidencia.Text);
-                    int n = cmd.ExecuteNonQuery();
-                    if(n > 0)
+                    switch (extension.ToLower())
                     {
-                        MessageBox.Show("Archivo subido correctamente");
-                        cmd.Dispose();
-                        conn.Close();
+                        case ".doc":
+                        case ".docx":
+                        case ".pdf":
+                        case ".png":
+                        case ".jpg":
+                        case ".txt":
+                            flag = 1;
+                            break;
+                        default:
+                            flag= 0;
+                            break;
+                    }
+                    if (flag == 1)
+                    {
+                        string query = "INSERT INTO archivo (id_archivo, nombre, id_incidencia) VALUES (id_archivo,@nombre,@id_incidencia)";
+
+                        MySqlConnection conn = con.Conectar();
+                        MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                        cmd.Parameters.AddWithValue("@nombre", nombre+extension);
+                        cmd.Parameters.AddWithValue("@id_incidencia", lb_IdIncidencia.Text);
+                        int n = cmd.ExecuteNonQuery();
+                        if (n > 0)
+                        {
+                            MessageBox.Show("Archivo subido correctamente");
+                            cmd.Dispose();
+                            conn.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se ha podido subir el archivo");
+                        }
+                        Directory.CreateDirectory(MapPath("~/Files/"+lb_IdIncidencia.Text));
+                        fuArchivo.SaveAs(Server.MapPath("~/Files/"+lb_IdIncidencia.Text+"/"+nombre+extension));
+                        ActualizaFchIncidencia();
+                        InsertaEnHistorial(3);
+                        Response.Redirect("DetalleIncidencia.aspx?id=" + lb_IdIncidencia.Text);
                     }
                     else
                     {
-                        MessageBox.Show("No se ha podido subir el archivo");
+                        MessageBox.Show("Solo están permitidos los archivos .doc, .docx, .pdf, .png, .jpg, .txt");
                     }
-                    Directory.CreateDirectory(MapPath("~/Files/"+lb_IdIncidencia.Text));
-                    fuArchivo.SaveAs(Server.MapPath("~/Files/"+lb_IdIncidencia.Text+"/"+nombre+extension));
-                    ActualizaFchIncidencia();
-                    InsertaEnHistorial(3);
-                    Response.Redirect("DetalleIncidencia.aspx?id=" + lb_IdIncidencia.Text);
                 }
                 else
                 {
-                    MessageBox.Show("Solo están permitidos los archivos .doc, .docx, .pdf, .png, .jpg, .txt");
+                    MessageBox.Show("Tamaño máximo superado");
                 }
             }
             else
